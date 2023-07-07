586eda1c350625f06e9c3065bf59fc4e758905d7
Giacomo Perlo <giacomo.perlo@edu.unito.it>
2023-07-07 16:46:54 +0200

    working flow decided; comment added

--- onnx/src/main.rs
+++ onnx/src/main.rs
@@ -1,3 +1,27 @@
+use protobuf::parse_from_bytes;
+use protobuf::Message;
+use std::str;
+
+/*
+
+TODO:
+1 -> leggere il .onnx
+    //08 -> 0000 1000
+    //wire_type = 0, field_number = 1
+    //ho gia creato un ModelProto, cerco il field con number = filed_number e creo la struct che mi dice(i.e. GraphProto)
+
+2 -> per ogni message in onnx.proto, va creata la struct corrispondente seguendo le annotations di onnx.proto,
+    fornendo la relativa implementazione
+
+3 -> riguardo alla correttezza sintattica possiamo seguire il principio per cui:
+        se io voglio creare un ModelProto all'interno di un GraphProto, cercherà di creare un'istanza di un oggetto che
+        non è disponibile nella struct GraphProto. Daremo lì l'errore.
+
+*/
+
+
 fn main() {
-    println!("Hello, world!");
+    // Leggi il contenuto del file ONNX in un buffer
+    //let onnx_bytes = std::fs::read("models/squeezenet1.0-8.onnx").expect("Failed to read file");
+    //println!("{:?}", onnx_bytes);
 }
